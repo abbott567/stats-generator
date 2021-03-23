3221dff47fe0d8fe647ced821fc25cf660b6329f
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2021-03-23 16:55:57 +0000

    removed console log from gulp task

--- gulp/build-tasks/zip.js
+++ gulp/build-tasks/zip.js
@@ -5,7 +5,6 @@ const { format } = require('date-fns')
 gulp.task('build:zip', function () {
   const date = format(new Date(), 'yyyy-MM-dd')
   const filename = `${date}-html-download.zip`
-  console.log(date)
   return gulp.src('./build/**/*')
     .pipe(zip(filename))
     .pipe(gulp.dest('./build'))
