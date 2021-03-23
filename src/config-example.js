aad35b000381b01c236d0e4f926c306aea7ecaa4
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2021-03-23 14:48:12 +0000

    Updated dummy config

--- src/config-example.js
+++ src/config-example.js
@@ -2,6 +2,8 @@ const { format } = require('date-fns')
 
 module.exports = {
   org_name: 'Organisation name',
+  org_repo: 'link to shared repo if needed',
+  a11y_report_link: 'link to accessibility audit for statement',
   date: format(new Date(), 'd MMMM yyyy'),
   contact_email: 'name@email.com'
 }
